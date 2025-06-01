a92e022bb79278acef352f3a7e1865d33209a4b1
Shounak <shounakpurnapatre1@gmail.com>
2025-06-01 22:28:19 +0530

    - Removed unused imports

--- src/components/FrameworkSelector.tsx
+++ src/components/FrameworkSelector.tsx
@@ -1,4 +1,3 @@
-import { useState } from "react";
 
 interface FrameworkSelectorProps {
   framework: string;
--- src/components/PackagingSelector.tsx
+++ src/components/PackagingSelector.tsx
@@ -1,4 +1,3 @@
-import { useState } from "react";
 
 interface PackagingSelectorProps {
   packaging: string;
